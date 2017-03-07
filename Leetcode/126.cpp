4925e230f50b30f9e6950e2b43f6051d227ee0ea
xcszbdnl <xcsbdnl@gmail.com>
2017-03-07 21:42:58 +0800

    leetcode 126

---
+++ Leetcode/126.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
+        map<string, int> word_to_idx, idx_to_word;
+
+
+    }
+};
+
+int main() {
+}
